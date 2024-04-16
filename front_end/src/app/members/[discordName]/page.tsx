6c103105321d5708c218ab3d093a471372d4e9ec
n1dddd <danielseniv@outlook.com>
2024-04-16 13:16:35 -0400

    Move to t3 stack nextjs start

--- front_end/src/app/members/[discordName]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function MemberDetails({ 
-    params,
-}: {
-    params: { discordName: string };
-}) {
-    return (
-        <h1>{params.discordName}</h1>
-      )
-}
--- front_end/src/app/tournaments/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Tournaments() {
-    return (
-      <h1>Tournaments</h1>
-    )
-  }

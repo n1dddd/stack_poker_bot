3a4690186cfa1d1ce3e6c9f7512248b0ba97d98b
n1dddd <danielseniv@outlook.com>
2024-04-20 01:38:06 -0400

    Add first api call to flask backend

--- front_end/src/app/page.tsx
+++ front_end/src/app/page.tsx
@@ -1,10 +1,16 @@
+"use client";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
+
+  useEffect(() => {
+    fetch('/api/flask/users')
+      .then(res => console.log(res.json()))
+  }) 
   return (
     <main className="">
       <div className="flex "></div>
-
     </main>
   );
 }

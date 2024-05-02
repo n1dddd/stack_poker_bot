70bf423d1d5194f7d15caea7bd0f7bfd17e6f46a
n1dddd <danielseniv@outlook.com>
2024-05-01 23:52:59 -0400

    Add page set-up to intake the member_list component

--- front_end/src/app/members/page.tsx
+++ front_end/src/app/members/page.tsx
@@ -1,3 +1,9 @@
-export default function Members() {
-    return <h1>Members</h1>
+import MemberList from "~/components/member_list";
+
+export default async function Members() {
+  return (
+    <main className="2xl:w-3/4 xl:px-0 px-4 w-full flex-col self-center justify-self-center py-8">
+      <MemberList />
+    </main>
+  );
 }

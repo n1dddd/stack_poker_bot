5e5339751905e341808fd1f303e4f6881f50acf8
n1dddd <danielseniv@outlook.com>
2024-05-02 23:58:06 -0400

    Import the tournaments_list component into Tournaments page

--- front_end/src/app/tournaments/page.tsx
+++ front_end/src/app/tournaments/page.tsx
@@ -1,3 +1,10 @@
+import TournamentList from "~/components/tournament_list"
+
 export default function Tournaments() {
-    return <h1>Tournaments</h1>
+    return (
+    <main className="2xl:w-3/4 xl:px-0 px-4 w-full flex-col self-center justify-self-center py-8">
+        <TournamentList />
+    </main>
+
+    )
 }

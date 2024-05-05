241b108d38ad1ddaa81cc9c0153243acf9a877ce
n1dddd <danielseniv@outlook.com>
2024-05-05 02:08:19 -0400

    Fix TypeScript error where .length property was not type matching to value that had an array

--- front_end/src/components/member_list.tsx
+++ front_end/src/components/member_list.tsx
@@ -4,11 +4,18 @@ interface MemberInfo {
     avatar_url: string,
     bankroll: number,
     username: string,
-    id: number
+    id: number,
+    tournaments_entered: number,
+    tournaments_won: Array<MemberTournamentInfo>,
+}
 
+interface MemberTournamentInfo {
+    id: number,
+    stake: number,
+    payout: number,
 }
 async function getUsers() {
-    const response = await fetch('http://localhost:4000/api/flask/users', { next: { revalidate: 900 } })
+    const response = await fetch('http://localhost:4000/api/flask/users', { cache: 'no-store' })
         .then(res => { return res.json() })
     return response;
 }
@@ -19,7 +26,7 @@ export default async function MemberList() {
     return (
         <div className="flex flex-row gap-8 flex-wrap justify-evenly">
             {member_list.map((member : MemberInfo) => (
-                <div key={member.id} className="flex flex-col bg-zinc-900 rounded-md md:w-52 w-4/5 items-center p-8">
+                <div key={member.id} className="flex flex-col bg-zinc-900 rounded-md md:w-64 w-4/5 items-center p-8 gap-4">
                     <Image
                         src={member.avatar_url}
                         width={75}
@@ -31,8 +38,8 @@ export default async function MemberList() {
                     />
 
                     <h1 className="text-xl">@{member.username}</h1>
-                    <h1 className="text-xl">${member.bankroll}</h1>
-                    Won Tournaments = 
+                    <h1 className="text-xl">Bankroll: ${member.bankroll}</h1>
+                    <h1 className="text-xl">Tournaments Won: {member.tournaments_won.length}</h1>
                 </div>
             ))}
         </div>

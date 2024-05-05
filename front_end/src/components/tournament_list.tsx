a58de79a7bc4e0b02d2258b9ab7e80ca54f78c66
n1dddd <danielseniv@outlook.com>
2024-05-05 02:48:49 -0400

    Remove simpleDateFunction and move it to helper file

--- front_end/src/components/tournament_list.tsx
+++ front_end/src/components/tournament_list.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Table from 'public/Table.png'
+import convertToSimpleTime from "~/helper";
 
 interface ParticipantsInfo {
     id: string,
@@ -24,21 +25,6 @@ interface MemberInfo {
 
 }
 
-function convertToSimpleTime(gmtTime) {
-    const date = new Date(gmtTime);
-    // Convert to EST time zone
-    const estTime = new Date(date.toLocaleString("en-US", { timeZone: "America/New_York" }));
-    // Get the components of the date
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const month = months[estTime.getMonth()];
-    const day = estTime.getDate();
-    const hours = estTime.getHours() > 12 ? estTime.getHours() - 12 : estTime.getHours(); // Convert to 12-hour format
-    const minutes = estTime.getMinutes();
-    const ampm = estTime.getHours() >= 12 ? 'PM' : 'AM'; // Get AM/PM
-    // Format the time components
-    const formattedTime = `${month} ${day} ${hours}:${minutes < 10 ? '0' : ''}${minutes} ${ampm}`;
-    return formattedTime;
-}
 
 async function getTournaments() {
     const response = await fetch('http://localhost:4000/api/flask/tournaments', { cache: "no-store" })
@@ -74,7 +60,6 @@ export default async function TournamentList() {
                                 </div>
                             ))}
                         </div>
-
                     </div>
                     <div className="flex flex-col gap-4 items-center justify-evenly">
                         <div className="flex flex-row gap-2">
@@ -89,8 +74,6 @@ export default async function TournamentList() {
                             <h1 className="md:text-3xl text-xl font-bold">Payout:</h1>
                             <h1 className="md:text-2xl text-lg self-end">${tournament.payout}</h1>
                         </div>
-
-
                     </div>
                 </div>
             ))}

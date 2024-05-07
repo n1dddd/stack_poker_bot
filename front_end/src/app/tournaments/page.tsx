406880f4e185762c831a64dec3449b2fecd01820
n1dddd <danielseniv@outlook.com>
2024-05-06 23:53:08 -0400

    Remove width property and move width sizing to parent container in layout for children and footer

--- front_end/src/app/tournaments/page.tsx
+++ front_end/src/app/tournaments/page.tsx
@@ -2,7 +2,7 @@ import TournamentList from "~/components/tournament_list"
 
 export default function Tournaments() {
     return (
-    <main className="2xl:w-3/4 xl:px-0 px-4 w-full flex-col self-center justify-self-center py-8">
+    <main className="xl:px-0 px-4 w-full flex-col self-center justify-self-center py-8">
         <TournamentList />
     </main>
 

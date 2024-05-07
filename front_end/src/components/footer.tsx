6003a35573df1ec5e57846e012e839b6ba5dc636
n1dddd <danielseniv@outlook.com>
2024-05-06 23:54:21 -0400

    Add very basic footer element

---
+++ front_end/src/components/footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <div className="xl:px-0 px-4 w-full flex-row self-center justify-self-center py-16">
+        This is the footer
+    </div>
+  )
+}

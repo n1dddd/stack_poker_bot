6fffc4edc9057513f7378aba2b55e914e9ba8d4f
n1dddd <danielseniv@outlook.com>
2024-05-06 23:52:38 -0400

    Remove width property and move width sizing to parent container in layout for children and footer

--- front_end/src/app/page.tsx
+++ front_end/src/app/page.tsx
@@ -2,7 +2,7 @@ import Podium from "~/components/podium";
 
 export default async function HomePage() {
   return (
-    <main className="2xl:w-3/4 xl:px-0 px-4 w-full flex-col self-center justify-self-center py-8">
+    <main className="xl:px-0 px-4 w-full flex-col self-center justify-self-center py-8">
       <Podium />
     </main>
   );

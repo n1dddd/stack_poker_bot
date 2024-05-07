241ed3f7383516d6f34c0882f6d0bff1f66be7fc
n1dddd <danielseniv@outlook.com>
2024-05-06 23:52:18 -0400

    Remove width property and move width sizing to parent container in layout for children and footer

--- front_end/src/app/members/page.tsx
+++ front_end/src/app/members/page.tsx
@@ -2,7 +2,7 @@ import MemberList from "~/components/member_list";
 
 export default async function Members() {
   return (
-    <main className="2xl:w-3/4 xl:px-0 px-4 w-full flex-col self-center justify-self-center py-8">
+    <main className="xl:px-0 px-4 w-full flex-col self-center justify-self-center py-8">
       <MemberList />
     </main>
   );
